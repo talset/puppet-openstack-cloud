1fd97800e33744e3bf8f3c27bdbe36eec8471562
Yanis Guenane <yanis.guenane@enovance.com>
2014-05-13 15:48:07 -0400

    compute: Add SSL configuration
    Allow compute SSL configuration

--- manifests/compute.pp
+++ manifests/compute.pp
@@ -39,6 +39,28 @@
 #   (optional) Password to connect to nova queues.
 #   Defaults to 'rabbitpassword'
 #
+# [*rabbit_use_ssl*]
+#   (optional) Connect over SSL for RabbitMQ
+#   Defaults to false
+#
+# [*kombu_ssl_ca_certs*]
+#   (optional) SSL certification authority file (valid only if SSL enabled).
+#   Defaults to undef
+#
+# [*kombu_ssl_certfile*]
+#   (optional) SSL cert file (valid only if SSL enabled).
+#   Defaults to undef
+#
+# [*kombu_ssl_keyfile*]
+#   (optional) SSL key file (valid only if SSL enabled).
+#   Defaults to undef
+#
+# [*kombu_ssl_version*]
+#   (optional) SSL version to use (valid only if SSL enabled).
+#   Valid values are TLSv1, SSLv23 and SSLv3. SSLv2 may be
+#   available on some distributions.
+#   Defaults to 'SSLv3'
+#
 # [*ks_glance_internal_host*]
 #   (optional) Internal Hostname or IP to connect to Glance API
 #   Defaults to '127.0.0.1'
@@ -67,13 +89,30 @@
 #   (optionnal) Memcached servers used by Keystone. Should be an array.
 #   Defaults to ['127.0.0.1:11211']
 #
-
+# [*ssl*]
+#   (optional) Enable SSL on the API server
+#   Defaults to false, not set
+#
+# [*ssl_cacert*]
+#   (required with ssl) CA certificate to use for SSL support.
+#
+# [*ssl_cert*]
+#   (required with ssl) Certificate to use for SSL support.
+#
+# [*ssl_key*]
+#   (required with ssl) Private key to use for SSL support.
+#
 class cloud::compute(
   $nova_db_host            = '127.0.0.1',
   $nova_db_user            = 'nova',
   $nova_db_password        = 'novapassword',
   $rabbit_hosts            = ['127.0.0.1:5672'],
   $rabbit_password         = 'rabbitpassword',
+  $rabbit_use_ssl          = false,
+  $kombu_ssl_ca_certs      = undef,
+  $kombu_ssl_certfile      = undef,
+  $kombu_ssl_keyfile       = undef,
+  $kombu_ssl_version       = 'SSLv3',
   $ks_glance_internal_host = '127.0.0.1',
   $glance_api_port         = 9292,
   $verbose                 = true,
@@ -85,7 +124,11 @@ class cloud::compute(
   $neutron_password        = 'neutronpassword',
   $neutron_region_name     = 'RegionOne',
   $memcache_servers        = ['127.0.0.1:11211'],
-  $availability_zone       = 'RegionOne'
+  $availability_zone       = 'RegionOne',
+  $ssl                     = false,
+  $ssl_cacert              = false,
+  $ssl_cert                = false,
+  $ssl_key                 = false,
 ) {
 
   if !defined(Resource['nova_config']) {
@@ -109,6 +152,11 @@ class cloud::compute(
     rabbit_userid       => 'nova',
     rabbit_hosts        => $rabbit_hosts,
     rabbit_password     => $rabbit_password,
+    rabbit_use_ssl      => $rabbit_use_ssl,
+    kombu_ssl_ca_certs  => $kombu_ssl_ca_certs,
+    kombu_ssl_certfile  => $kombu_ssl_certfile,
+    kombu_ssl_keyfile   => $kombu_ssl_keyfile,
+    kombu_ssl_version   => $kombu_ssl_version,
     glance_api_servers  => "http://${ks_glance_internal_host}:${glance_api_port}",
     memcached_servers   => $memcache_servers,
     verbose             => $verbose,
@@ -116,13 +164,18 @@ class cloud::compute(
     log_dir             => $log_dir,
     log_facility        => $log_facility,
     use_syslog          => $use_syslog,
+    use_ssl             => $ssl,
+    ca_file             => $ssl_cacert,
+    cert_file           => $ssl_cert,
+    key_file            => $ssl_key,
   }
 
   class { 'nova::network::neutron':
-      neutron_admin_password => $neutron_password,
-      neutron_admin_auth_url => "${neutron_protocol}://${neutron_endpoint}:35357/v2.0",
-      neutron_url            => "${neutron_protocol}://${neutron_endpoint}:9696",
-      neutron_region_name    => $neutron_region_name
+      neutron_admin_password       => $neutron_password,
+      neutron_admin_auth_url       => "${neutron_protocol}://${neutron_endpoint}:35357/v2.0",
+      neutron_url                  => "${neutron_protocol}://${neutron_endpoint}:9696",
+      neutron_region_name          => $neutron_region_name,
+      neutron_ca_certificates_file => $ssl_cacert,
   }
 
   nova_config {
